5530f1c823c9fd23ac54410c71a012fd82d12bd7
Chase Dunagan <cdunagan@utexas.edu>
2017-01-06 11:57:23 -0700

    add view stuff

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,5 @@
 class SearchController < ApplicationController
   def index
-    # response = Faraday.get "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=distance,storeType,longName,city,phone&pageSize=15&apiKey=#{ENV['API_KEY']}"
-    # @rep = JSON.parse(response.body)
     @stores = BestBuy.stores
-    binding.pry
   end
 end
--- spec/features/user_sees_closest_stores_spec.rb
+++ spec/features/user_sees_closest_stores_spec.rb
@@ -15,11 +15,16 @@ RSpec.describe "User enters a zipcode" do
 
     # And I should see stores within 25 miles of 80202
     # And I should see a message that says "16 Total Stores"
-    expect(page).to have_content("16 Total Stores")
+    # expect(page).to have_content("16 Total Stores")
 
     expect(page).to have_content("Best Buy Mobile - Cherry Creek Shopping Center")
+    expect(page).to have_content("3.25")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("303-270-9189")
+    expect(page).to have_content("Mobile")
     expect(page).to have_content("Best Buy - Parker")
-    # And I should see exactly 15 results
-    # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+
+    expect(page).to have_content("15. Store Name:")
+    expect(page).to_not have_content("16. Store Name:")
   end
 end
